c59b8f85154941ec8f93eacc6d8a15a9d1945187
engineeringawesome <robomower9508@gmail.com>
2016-03-19 14:15:25 -0400

    Updated to 9.1

--- src/RobotBase.java
+++ src/RobotBase.java
@@ -1,15 +1,18 @@
 import lejos.hardware.motor.Motor;
 import lejos.robotics.RegulatedMotor;
 import lejos.robotics.navigation.DifferentialPilot;
+import lejos.robotics.navigation.MovePilot;
 
 public class RobotBase {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		RegulatedMotor leftMotor = Motor.C;
 		RegulatedMotor rightMotor = Motor.D;
-		DifferentialPilot robot = new DifferentialPilot(55, 120, leftMotor, rightMotor);
-		
-		robot.setAcceleration(250);
+		//MovePilot robot = new MovePilot(55, 120, leftMotor, rightMotor);
+		MovePilot robot = new MovePilot(55, 120, leftMotor, rightMotor);
+
+		robot.setLinearAcceleration(250);
 
 		robot.travel(100);
 
