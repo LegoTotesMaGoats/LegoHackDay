696d9d8d4d21dd62de86ce2a60385261d2372ee4
Peter Mitrano <mitranopeter@gmail.com>
2016-03-19 17:02:28 -0400

    it moves

--- src/one/One.java
+++ src/one/One.java
@@ -1,11 +1,15 @@
 package one;
 import common.Robot;
+import lejos.hardware.lcd.LCD;
 
 public class One extends Robot {
 
 	@Override
 	public void main() {
-		chassis.setSpeed(100, 0);
+		LCD.drawString("ROBOT ONE", 0, 0);
+		chassis.setVelocity(100, 0);
+		chassis.travel(100);
+		chassis.waitComplete();
 	}
 	
 }
