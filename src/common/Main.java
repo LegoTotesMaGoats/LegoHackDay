3da3e96d20da9b44b4d752d1517b023401fedcea
EngineeringAwesome <robomower9508@gmail.com>
2016-03-19 18:33:03 -0400

    Automatically select the right code to run based on EV3 name

--- src/common/Main.java
+++ src/common/Main.java
@@ -1,13 +1,22 @@
 package common;
 
+import lejos.hardware.ev3.LocalEV3;
 import one.One;
+import twentytwo.TwentyTwo;
 
 public class Main {
 
 	public static void main(String[] args) {
-
-		Robot robot = new One();
-		robot.main();
+		String name = LocalEV3.get().getName();
+		Robot robot = null;
+		if(name.equalsIgnoreCase("gompie")) {
+			robot = new One();
+		} else if(name.equalsIgnoreCase("ev22")) {
+			robot = new TwentyTwo();
+		}
+		if(robot != null) {
+			robot.main();
+		}
 	}
 
 }
